df0ce8e10b4abfa22d39142da822f855906b90e5
Nicholas Roberto <njr444@gmail.com>
2020-09-08 10:25:53 -0700

    added helper functions for password/email state and submit. created additional sign up fields component for sign up with email button click.

--- rent-away/src/pages/LogIn/SignUp.js
+++ rent-away/src/pages/LogIn/SignUp.js
@@ -5,6 +5,46 @@ import { bindActionCreators } from 'redux'
 import openModal from '../../actions/openModal'
 
 class SignUp extends Component {
+  constructor() {
+    super()
+    this.state = {
+      lowerPartOfForm: (
+        <button
+          type='button'
+          onClick={this.showInputs}
+          className='sign-up-button'
+        >
+          Sign up with email
+        </button>
+      ),
+      password: '',
+      email: ''
+    }
+  }
+
+  changeEmail = e => {
+    this.setState({ email: e.target.value })
+  }
+  changePassword = e => {
+    this.setState({ password: e.target.value })
+  }
+
+  submitLogin = e => {
+    e.preventDefault()
+    console.log(this.state.email)
+    console.log(this.state.password)
+  }
+
+  showInputs = () => {
+    this.setState({
+      lowerPartOfForm: (
+        <SignUpInputFields
+          changeEmail={this.changeEmail}
+          changePassword={this.changePassword}
+        />
+      )
+    })
+  }
   render() {
     return (
       <div className='login-form'>
@@ -15,7 +55,7 @@ class SignUp extends Component {
             <span>or</span>
             <div className='or-divider'></div>
           </div>
-          <button className='sign-up-button'>Sign up with email</button>
+          {this.state.lowerPartOfForm}
           <div className='divider'></div>
           <div>
             Already have an account?
@@ -51,3 +91,31 @@ const mapDispatchToProps = dispatch => {
 }
 
 export default connect(null, mapDispatchToProps)(SignUp)
+
+const SignUpInputFields = props => {
+  return (
+    <div className='sign-up-wrapper'>
+      <div className=' col m12'>
+        <div className='input-field' id='email'>
+          <div className='form-label'>Email</div>
+          <input type='text' placeholder='Email' onChange={props.changeEmail} />
+        </div>
+      </div>
+      <div className=' col m12'>
+        <div className='input-field' id='password'>
+          <div className='form-label'>Password</div>
+          <input
+            type='password'
+            placeholder='Password'
+            onChange={props.changePassword}
+          />
+        </div>
+      </div>
+      <div className='col m12'>
+        <button type='submit' className='btn red accent-2'>
+          Sign Up!
+        </button>
+      </div>
+    </div>
+  )
+}
