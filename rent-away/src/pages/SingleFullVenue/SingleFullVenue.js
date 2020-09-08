6a1296c0361039db41c157e870f944728a92863a
Nicholas Roberto <njr444@gmail.com>
2020-09-08 08:13:07 -0700

    added box for reservations with dates, num of guests, price per day and ratings. Also added details and amenities sections for venue.

--- rent-away/src/pages/SingleFullVenue/SingleFullVenue.js
+++ rent-away/src/pages/SingleFullVenue/SingleFullVenue.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import './SingleFullVenue.css'
+import Point from './Point'
 import axios from 'axios'
 
 class SingleFullVenue extends Component {
   state = {
-    singleVenue: {}
+    singleVenue: {},
+    points: [],
+    checkIn: '',
+    checkOut: '',
+    numberOfGuests: []
   }
 
   async componentDidMount() {
@@ -12,9 +17,31 @@ class SingleFullVenue extends Component {
     const url = `${window.apiHost}/venue/${vId}`
     const axiosResponse = await axios.get(url)
     const singleVenue = axiosResponse.data
-    this.setState({ singleVenue })
+
+    const pointsUrl = `${window.apiHost}/points/get`
+    const axiosPointsResponse = await axios.get(pointsUrl)
+
+    const points = singleVenue.points.split(',').map((point, index) => {
+      return (
+        <Point key={index} pointDesc={axiosPointsResponse.data} point={point} />
+      )
+    })
+    this.setState({ singleVenue, points })
   }
 
+  changeNumberOfGuests = e => {
+    this.setState({ numberOfGuests: e.target.value })
+  }
+  changeCheckIn = e => {
+    this.setState({ checkIn: e.target.value })
+  }
+  changeCheckOut = e => {
+    this.setState({ checkOut: e.target.value })
+  }
+
+  reserveNow = e => {
+    console.log('User wants to reserve')
+  }
   render() {
     const sv = this.state.singleVenue
     return (
@@ -23,11 +50,50 @@ class SingleFullVenue extends Component {
           <img src={sv.imageUrl} alt='venue' />
         </div>
         <div className='col s8 location-details offset-s2'>
-          <div className='location'>{sv.location}</div>
-          <div className='title'>{sv.title}</div>
-          <div className='guests'>{sv.guests}</div>
+          <div className='col s8 left-details'>
+            <div className='location'>{sv.location}</div>
+            <div className='title'>{sv.title}</div>
+            <div className='guests'>{sv.guests}</div>
 
-          <div className="divider"></div>
+            <div className='divider'></div>
+
+            {this.state.points}
+
+            <div className='details'>{sv.details}</div>
+            <div className='amenities'>{sv.amenities}</div>
+          </div>
+
+          <div className='col s4 right-details'>
+            <div className='price-per-night'>
+              ${sv.pricePerNight} <span>per day</span>
+            </div>
+            <div className='rating'>{sv.rating}</div>
+            <div className='col s6'>
+              Check-In
+              <input type='date' />
+            </div>
+            <div className='col s6'>
+              Check-Out
+              <input type='date' />
+            </div>
+            <div className='col s12'>
+              <select name='Number of Guests' className='browser-default'>
+                <option value='1 Guest'>1 Guest </option>
+                <option value='2 Guest'>2 Guests</option>
+                <option value='3 Guest'>3 Guests</option>
+                <option value='4 Guest'>4 Guests</option>
+                <option value='5 Guest'>5 Guests</option>
+                <option value='6 Guest'>6 Guests</option>
+                <option value='7 Guest'>7 Guests</option>
+                <option value='8 Guest'>8 Guests</option>
+              </select>
+            </div>
+            <div className='col s12 center'>
+              <button onClick={this.reserveNow} className='btn red accent-2'>
+                Reserve
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     )
