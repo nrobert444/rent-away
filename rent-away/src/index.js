7128b3abacd99bbc26ad3a5744e18d69e15d3954
Nicholas Roberto <njr444@gmail.com>
2020-09-08 12:37:42 -0700

    added redux persist to app to persist state through page refreshes.

--- rent-away/src/index.js
+++ rent-away/src/index.js
@@ -2,16 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
+import Spinner from './utilities/Spinner/Spinner'
+
+// redux setup
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import reduxPromise from 'redux-promise'
 import rootReducer from './reducers/rootReducer'
 
-const store = applyMiddleware(reduxPromise)(createStore)(rootReducer)
+// redux persist setup
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib.storage'
+import { PersistGate } from 'redux-persist/integration/react'
+import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2'
+
+const persistConfig = {
+  key: 'root',
+  stateReconciler: autoMergeLevel2,
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+const store = applyMiddleware(reduxPromise)(createStore)(persistedReducer)
+const persistor = persistStore(store)
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor} loading={spinner}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 )
