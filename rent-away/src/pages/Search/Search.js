20f3b60440db8f94c06ea8d011791074138547b9
Nicholas Roberto <njr444@gmail.com>
2020-09-10 10:12:25 -0700

    refactored search component to use useState, useEffect hooks.

--- rent-away/src/pages/Search/Search.js
+++ rent-away/src/pages/Search/Search.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Search.css'
 import '../Home/Home.css'
 import Cities from '../../utilities/City/Cities'
@@ -7,54 +7,45 @@ import Activities from '../../utilities/Activity/Activities'
 import axios from 'axios'
 import Spinner from '../../utilities/Spinner/Spinner'
 
-class Search extends Component {
-  state = {
-    activities: [],
-    cities: [],
-    venues: [],
-    apiResponse: false
-  }
-  async componentDidMount() {
-    const searchTerm = this.props.match.params.searchTerm
-    const url = `${window.apiHost}/search/${searchTerm}`
-    const resp = await axios.get(url)
-    console.log(resp.data)
-    this.setState({
-      activities: resp.data.activities,
-      cities: resp.data.cities,
-      venues: resp.data.venues,
-      apiResponse: true
-    })
-  }
+const Search = props => {
+  const [activities, setActivities] = useState([])
+  const [cities, setCities] = useState([])
+  const [venues, setVenues] = useState([])
+  const [apiResponse, setApiResponse] = useState(false)
 
-  render() {
-    if (!this.state.apiResponse) {
-      return <Spinner />
+  useEffect(() => {
+    const fetchSearchData = async () => {
+      const searchTerm = props.match.params.searchTerm
+      const url = `${window.apiHost}/search/${searchTerm}`
+      const resp = await axios.get(url)
+      setActivities(resp.data.activities)
+      setCities(resp.data.cities)
+      setVenues(resp.data.venues)
+      setApiResponse(true)
     }
-    return (
-      <div className='container-fluid lower-fold'>
-        <div className='row'>
-          <div className='col s12'>
-            <Cities
-              cities={this.state.cities}
-              header='Cities Matching Your Search'
-            />
-          </div>
-          <div className='col s12'>
-            <Activities
-              activities={this.state.activities}
-              header='Activies Matching Your Search'
-            />
-          </div>
-          <div className='col s12'>
-            <Venues
-              venues={this.state.venues}
-              header='Venues matching your search'
-            />
-          </div>
+    fetchSearchData()
+  }, [])
+
+  if (!apiResponse) {
+    return <Spinner />
+  }
+  return (
+    <div className='container-fluid lower-fold'>
+      <div className='row'>
+        <div className='col s12'>
+          <Cities cities={cities} header='Cities Matching Your Search' />
+        </div>
+        <div className='col s12'>
+          <Activities
+            activities={activities}
+            header='Activies Matching Your Search'
+          />
+        </div>
+        <div className='col s12'>
+          <Venues venues={venues} header='Venues matching your search' />
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 export default Search
