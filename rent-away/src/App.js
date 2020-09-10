6681f00eaf917f50e64c6ed99dbacec19f488b80
Nicholas Roberto <njr444@gmail.com>
2020-09-10 15:29:05 -0700

    converted app to functional component.

--- rent-away/src/App.js
+++ rent-away/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Navbar from './utilities/Navbar/Navbar'
@@ -10,25 +10,23 @@ import CityVenues from './pages/CityVenues/CityVenues'
 import PaymentSuccess from './pages/PaymentSuccess/PaymentSuccess'
 import Search from './pages/Search/Search'
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Route path='/' component={Navbar} />
-        <Route exact path='/' component={Home} />
-        <Route exact path='/venue/:vid' component={SingleFullVenue} />
-        <Route exact path='/city/:cityName' component={CityVenues} />
-        <Route
-          exact
-          path='/payment-success/:stripeToken'
-          component={PaymentSuccess}
-        />
-        <Route path='/account' component={Account} />
-        <Route exact path='/search/:searchTerm' component={Search} />
-        <Route path='/' component={Modal} />
-      </Router>
-    )
-  }
+const App = props => {
+  return (
+    <Router>
+      <Route path='/' component={Navbar} />
+      <Route exact path='/' component={Home} />
+      <Route exact path='/venue/:vid' component={SingleFullVenue} />
+      <Route exact path='/city/:cityName' component={CityVenues} />
+      <Route
+        exact
+        path='/payment-success/:stripeToken'
+        component={PaymentSuccess}
+      />
+      <Route path='/account' component={Account} />
+      <Route exact path='/search/:searchTerm' component={Search} />
+      <Route path='/' component={Modal} />
+    </Router>
+  )
 }
 
 export default App
