29511710b3d5f42824718f4c1bede6ae2b289baa
Nicholas Roberto <njr444@gmail.com>
2020-09-10 13:46:32 -0700

    changed account to functional componentwith useEffect handling data fetch and useSelector replacing connect.

--- rent-away/src/pages/Account/Account.js
+++ rent-away/src/pages/Account/Account.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import './Account.css'
 import axios from 'axios'
 import moment from 'moment'
@@ -8,64 +8,60 @@ import Bookings from './Bookings'
 import ChangePassword from './ChangePassword'
 import AccountSideBar from './AccountSideBar'
 
-class Account extends Component {
-  state = {
-    pastBookings: [],
-    upcomingBookings: []
-  }
+const Account = props => {
+  const token = useSelector(state => state.auth.token)
+  const [pastBookings, setPastBookings] = useState([])
+  const [upcomingBookings, setUpcomingBookings] = useState([])
 
-  async componentDidMount() {
+  useEffect(() => {
+    const data = { token }
     const accountUrl = `${window.apiHost}/users/getBookings`
-    const data = { token: this.props.auth.token }
-    const resp = await axios.post(accountUrl, data)
-    console.log(resp.data)
-    let pastBookings = [],
-      upcomingBookings = []
-    resp.data.forEach(booking => {
-      const today = moment()
-      const checkOutDate = moment(booking.checkOut)
-      const diffDays = checkOutDate.diff(today, 'days')
-      if (diffDays < 0) {
-        pastBookings.push(booking)
-      } else {
-        upcomingBookings.push(booking)
-      }
-    })
-    this.setState({ pastBookings, upcomingBookings })
-  }
-  render() {
-    const { pastBookings, upcomingBookings } = this.state
-    return (
-      <div className='account container-fluid'>
-        <AccountSideBar />
-        <div className='row'>
-          <div className='col s8 offset-s3'>
-            <Route
-              exact
-              path='/account'
-              render={() => <h1>Choose an option on the Left!</h1>}
+    const fetchAccountData = async () => {
+      const resp = await axios.post(accountUrl, data)
+      console.log(resp.data)
+      let pastBookings = [],
+        upcomingBookings = []
+      resp.data.forEach(booking => {
+        const today = moment()
+        const checkOutDate = moment(booking.checkOut)
+        const diffDays = checkOutDate.diff(today, 'days')
+        if (diffDays < 0) {
+          pastBookings.push(booking)
+        } else {
+          upcomingBookings.push(booking)
+        }
+      })
+      setPastBookings(pastBookings)
+      setUpcomingBookings(upcomingBookings)
+    }
+    fetchAccountData()
+  }, [token])
+
+  return (
+    <div className='account container-fluid'>
+      <AccountSideBar />
+      <div className='row'>
+        <div className='col s8 offset-s3'>
+          <Route
+            exact
+            path='/account'
+            render={() => <h1>Choose an option on the Left!</h1>}
+          />
+          <Route path='/account/reservations/confirmed'>
+            <Bookings
+              type='upcoming'
+              bookings={upcomingBookings}
+              token={token}
             />
-            <Route path='/account/reservations/confirmed'>
-              <Bookings
-                type='upcoming'
-                bookings={upcomingBookings}
-                token={this.props.auth.token}
-              />
-            </Route>
-            <Route path='/account/reservations/past'>
-              <Bookings type='past' bookings={pastBookings} />
-            </Route>
-            <Route path='/account/change-pass' component={ChangePassword} />
-          </div>
+          </Route>
+          <Route path='/account/reservations/past'>
+            <Bookings type='past' bookings={pastBookings} token={token} />
+          </Route>
+          <Route path='/account/change-pass' component={ChangePassword} />
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
-const mapStateToProps = state => {
-  return {
-    auth: state.auth
-  }
-}
-export default connect(mapStateToProps)(Account)
+export default Account
--- rent-away/src/pages/Home/SearchBox.js
+++ rent-away/src/pages/Home/SearchBox.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './SearchBox.css'
 import useControlledInput from '../../customHooks/useControlledInput'
 
